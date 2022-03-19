a7dfa02c2be7d16fd18f77edafc2d6c48c8973b2
Brian Shu <littlebubu.shu@gmail.com>
2022-03-19 13:29:00 -0400

    .

--- bench/Main.hs
+++ bench/Main.hs
@@ -11,45 +11,45 @@ main :: IO ()
 main =
   defaultMainWith
     defaultConfig
-    [copyBenches]
+    [undefined]
 
-copyBenches :: Benchmark
-copyBenches =
-  bgroup
-    "copy"
-    [ copyBenchesWithArr (Array.run $ Array.new_ i)
-      | i <- arrSizes
-    ]
-{-# NOINLINE copyBenches #-}
+-- copyBenches :: Benchmark
+-- copyBenches =
+--   bgroup
+--     "copy"
+--     [ copyBenchesWithArr (Array.run $ Array.new_ i)
+--       | i <- arrSizes
+--     ]
+-- {-# NOINLINE copyBenches #-}
 
-arrSizes :: [Int]
-arrSizes = [1, 16, 32]
+-- arrSizes :: [Int]
+-- arrSizes = [1, 16, 32]
 
-copyBenchesWithArr :: SmallArray Int -> Benchmark
-copyBenchesWithArr !arr =
-  bgroup
-    ("size " ++ show (Array.length arr))
-    [ copyBench i arr
-      | i <- take 5 amounts
-    ]
-{-# NOINLINE copyBenchesWithArr #-}
+-- copyBenchesWithArr :: SmallArray Int -> Benchmark
+-- copyBenchesWithArr !arr =
+--   bgroup
+--     ("size " ++ show (Array.length arr))
+--     [ copyBench i arr
+--       | i <- take 5 amounts
+--     ]
+-- {-# NOINLINE copyBenchesWithArr #-}
 
-copyBench :: Int -> SmallArray Int -> Benchmark
-copyBench amount !arr = bench (show amount) $ whnf (copyN amount) arr
-{-# NOINLINE copyBench #-}
+-- copyBench :: Int -> SmallArray Int -> Benchmark
+-- copyBench amount !arr = bench (show amount) $ whnf (copyN amount) arr
+-- {-# NOINLINE copyBench #-}
 
-copyN :: Int -> SmallArray a -> SmallArray a
-copyN i arr
-  | i < 0 = error "less than zero"
-  | otherwise = foldl' (\arr _i -> copy arr) arr [1 .. i]
-{-# INLINE copyN #-}
+-- copyN :: Int -> SmallArray a -> SmallArray a
+-- copyN i arr
+--   | i < 0 = error "less than zero"
+--   | otherwise = foldl' (\arr _i -> copy arr) arr [1 .. i]
+-- {-# INLINE copyN #-}
 
-copy :: SmallArray a -> SmallArray a
-copy arr = Array.create_ len $ \marr -> do
-  Array.copy marr 0 arr 0 len
-  where
-    len = Array.length arr
-{-# INLINE copy #-}
+-- copy :: SmallArray a -> SmallArray a
+-- copy arr = Array.create_ len $ \marr -> do
+--   Array.copy marr 0 arr 0 len
+--   where
+--     len = Array.length arr
+-- {-# INLINE copy #-}
 
-amounts :: [Int]
-amounts = [10 ^ i | i <- [0 :: Int ..]]
+-- amounts :: [Int]
+-- amounts = [10 ^ i | i <- [0 :: Int ..]]
