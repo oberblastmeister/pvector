0c405415c6157faf332f96b58f8eb06dafe07430
brian <littlebubu.shu@gmail.com>
2022-07-04 14:16:08 -0400

    add comparison docs and diagram

--- src/Data/Vector/Persistent.hs
+++ src/Data/Vector/Persistent.hs
@@ -1,3 +1,23 @@
+-- |
+-- The @'Vector' a@ type is an persistent vector of elements of type @a@.
+--
+-- This module should be imported qualified, to avoid name clashes with the "Prelude".
+-- 
+-- Many operations have a average-case complexity of /O(log n)/.  The
+-- implementation uses a large base (i.e. 32) so in practice these
+-- operations are constant time.
+--
+-- = Comparison with Data.RRBVector and Data.Sequence
+-- * Persistent vectors generally have less operations than sequences or RRBVectors but those operations can be faster.
+-- * Persistent vectors have the fastest indexing.
+-- * Persistent vectors are faster than RRBVectors at snocing because of tail optimization.
+--   Snocing is a near constant time operation.
+--   Snocing is still slower than sequences.
+-- * RRBVectors are faster than persistent vectors at splitting and merging, but still slower than sequences.
+-- * RRBVectors are faster than Sequences at indexing but slower than persistent vectors.
+-- * Sequences have the fastest consing, snocing, and merging, but the slowest indexing.
+-- 
+-- ![diagram](docs/diagram.png)
 module Data.Vector.Persistent
   ( foldr,
     foldr',
